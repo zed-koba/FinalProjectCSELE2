44fb9c51fc96df431296a02bbd3b45e060cea387
zed-koba <cedricvhonpidlaoan@gmail.com>
2025-05-14 21:38:47 +0800

    Added models for post,like,comment

---
+++ LoginRegistration/Model/LikeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginRegistration.Model
+{
+    public class LikeModel
+    {
+        public List<string> userIdLikes;
+    }
+}
---
+++ LoginRegistration/Model/PostModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginRegistration.Model
+{
+    public class PostModel
+    {
+        public string postId { get; set; }
+        public string imageSource { get; set; }
+        public string caption { get; set; }
+        public string timeStamp { get; set; }
+        public ObservableCollection<CommentModel> comments { get; set; }
+        public ObservableCollection<LikeModel> like { get; set; }
+        public int noOfComments { get; set; }
+    }
+}
